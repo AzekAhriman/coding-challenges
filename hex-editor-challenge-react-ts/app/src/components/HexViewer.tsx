24579b63db0fe831310e893a604cc6d2e620f356
Azek <max105@list.ru>
2022-04-04 20:39:23 +0300

    Created basic hex view functional

--- hex-editor-challenge-react-ts/app/src/components/HexViewer.tsx
+++ hex-editor-challenge-react-ts/app/src/components/HexViewer.tsx
@@ -5,13 +5,34 @@ interface HexViewerProps {
 }
 
 export default function HexViewer(props: HexViewerProps) {
-  /*
-   * This component is the main challenge. You can be wild here and change
-   * everything!
-   */
+  const lines: React.ReactElement[] = [];
+  const BYTES_PER_LINE = 16;
+  const toHex = (n: string | number, l: number) => typeof n === "string" ? Number(n.charCodeAt(0)).toString(16) : n.toString(16).padStart(l, '0');
+
+  for (let offset = 0; offset < props.data.length; offset += 16) {
+    const slice = [...props.data.slice(offset, offset + BYTES_PER_LINE)];
+    const bytes =  slice.map((byte, i) => {
+      return <span key={offset + i}>{toHex(byte, 2)}</span>
+    });
+    const bytesComponent = <div>{bytes}</div>;
+    const asciiComponent = <span>|{
+      slice.map(byte => {
+        if(typeof byte === "string") {
+          return byte;
+        }
+        if (byte >=0x20 && byte < 0x7f) {
+          return String.fromCharCode(byte);
+        }
+        return '.';
+      })
+    }|</span>
+
+    lines.push(<div>{bytesComponent} {asciiComponent}</div>)
+  }
+
   return (
     <pre style={{ overflowWrap: 'break-word', whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
-      Here comes the HexViewer<br />{ props.data }
+      Here comes the HexViewer<br />{ lines }
     </pre>
   );
 }
